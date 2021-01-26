20a5e953475e4c9512952e630df3ddb625e5f9ba
Chad Hicks <chadjhicks@gmail.com>
2021-01-26 10:13:10 -0500

    add mars image

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ martianWeather
 
 https://caniuse.com/fetch
 
-
+Mark Watney's journey:
+https://trek.nasa.gov/mars/#v=0.1&x=-5.650409457092962&y=7.059268445186461&z=3&p=urn%3Aogc%3Adef%3Acrs%3AEPSG%3A%3A104905&d=&l=MC11E_HRMOSCO_COL%2Ctrue%2C1&l=ESP_040776_2115_RED_A_01_ORTHO%2Ctrue%2C1&l=ESP_042647_1760_RED_B_01_ORTHO%2Ctrue%2C1&l=ESP_042252_1930_RED_B_01_ORTHO%2Ctrue%2C1&l=InSightCTXMosaic_112917%2Ctrue%2C1&l=curiosity_hirise_mosaic%2Ctrue%2C1&locale=&b=mars&e=-47.22267430662194%2C-35.9631916897912%2C35.921855392436015%2C50.081728580164125&sfz=&w=
 
  */
