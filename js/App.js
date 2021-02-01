8c72f2a27fa564101adc090fbb4a44b51cc968e4
Chad Hicks <chadjhicks@gmail.com>
2021-02-01 13:38:18 -0500

    A few naming updates

--- js/App.js
+++ js/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import mars from '../img/mars-landing-curiosity-artist-e1543073738523.jpeg';
 import ResourceLinks from './ResourceLinks';
+import DataDisplay from './DataDisplay';
 
 const App = () => {
     return (
         <div>
-            <h2>How's the weather on Mars?</h2>
+            <h2>Martian Weather</h2>
 
             <img className="mars" src={mars} alt="mars" />
 
-            <h2>Resources</h2>
+            <DataDisplay />
+
             <ResourceLinks />
 
         </div>
