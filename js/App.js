4df57a3609392210da5b6f657d381e7ad7eb25bf
Chad Hicks <chadjhicks@gmail.com>
2021-11-14 11:41:41 -0500

    add apod image

--- js/App.js
+++ js/App.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import mars from '../img/mars-landing-curiosity-artist-e1543073738523.jpeg';
 import ResourceLinks from './ResourceLinks';
 import DataDisplay from './DataDisplay';
+import Apod from './Apod';
 
 const App = () => {
     return (
         <div>
-            <h2>Martian Weather</h2>
+            <h2>NASA API experiments</h2>
 
-            <img className="mars" src={mars} alt="mars" />
+            <Apod />
 
             <DataDisplay />
 
